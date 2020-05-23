7a549afaaac462f23b6f5935e20ab04c7cc05cbe
TachoArancibia <53100210+TachoArancibia@users.noreply.github.com>
2020-05-23 11:45:35 -0400

    Comment Echo

--- db.php
+++ db.php
@@ -9,10 +9,10 @@ $dbname = 'foro';
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-if($conn){
-     echo "Conexión con base de datos establecida.";
-} else {
-    echo "No se ha podido conectar a la base datos.";
-}
+//if($conn){
+//     echo "Conexión con base de datos establecida.";
+//} else {
+//    echo "No se ha podido conectar a la base datos.";
+//}
 
 ?>
