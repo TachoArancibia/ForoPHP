c899abdb7a4585ea5e2febe82c2d2580912545a9
TachoArancibia <jose.arancibia.cabrera@gmail.com>
2020-05-26 11:55:30 -0400

    Echo Path Writable v.5

--- Vusuario.php
+++ Vusuario.php
@@ -1,9 +1,10 @@
 <?php 
 include 'includes/header.php';
 include 'includes/navbarUsuario.php';
+include 'db.php';
 ?>
 <?php 
-include 'db.php';
+session_start();
 $varUsuario = $_SESSION['usuario_enSesion'];
 if($varUsuario == null || $varUsuario == ''){
     echo "No tienes autorización para esta vista.";
@@ -20,7 +21,6 @@ if($varUsuario == null || $varUsuario == ''){
                 <small> ¿Qué desear hacer? </small>
             </h2>
             <?php
-                include 'db.php'; 
                 $query = "SELECT * FROM usuarios WHERE usuario = '$varUsuario'";
                 $usuario = mysqli_query($conn,$query);
                 while($dato = mysqli_fetch_array($usuario)){
